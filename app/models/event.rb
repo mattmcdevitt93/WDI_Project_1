01717e8a7ca5a56cc9897b58356161c3f1712a53
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-10-21 15:12:10 -0400

    David, created scopes on events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,13 @@
 class Event < ActiveRecord::Base
   has_many :jobs
-  # scopes
 
-  # If current time is greater than
+  # You can chain these
+  # lookup scopes here: http://api.rubyonrails.org/classes/ActiveRecord/Scoping/Named/ClassMethods.html
+  scope :not_expired, -> { where('event_time_start > ?', Time.now)}
+  scope :overbooked, -> { where('slots < jobs_count') }
+  scope :full, -> { where('slots = jobs_count') }
+
+  # Event.already_applied(User.first)
+  scope :already_applied, -> (user) { joins(:jobs).where('user_id = ?', user.id) }
+
 end
