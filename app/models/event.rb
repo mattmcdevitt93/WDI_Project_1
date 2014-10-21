63c8eaf7191b7e98ae8c20906590e379c6c6624b
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-10-21 15:19:52 -0400

    Created instance methods on Events to ask questions about them, David

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,4 +10,22 @@ class Event < ActiveRecord::Base
   # Event.already_applied(User.first)
   scope :already_applied, -> (user) { joins(:jobs).where('user_id = ?', user.id) }
 
+
+  # Methods for individual events
+  def expired?
+    event_time_start > Time.now
+  end
+
+  def overbooked?
+    slots < jobs_count
+  end
+
+  def full?
+    slots == jobs_count
+  end
+
+  def already_applied?(user)
+    jobs.pluck(:user_id).include? user.id
+  end
+
 end
--- db/migrate/20141021184010_add_jobs_count_to_events.rb
+++ db/migrate/20141021184010_add_jobs_count_to_events.rb
@@ -1,5 +1,5 @@
 class AddJobsCountToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :jobs_count, :integer, index: true
+    add_column :events, :jobs_count, :integer, index: true, default: 0
   end
 end
