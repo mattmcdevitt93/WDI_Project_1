efd775dc59cfcb07603e69a5b0242f6d7d21b39b
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-10-22 11:32:08 -0400

    heroku fix 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_job, only: [:show, :edit, :update, :destroy]
+    # before_action :set_job, only: [:show, :edit, :update, :destroy]
 
 
   def index
